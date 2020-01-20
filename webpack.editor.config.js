6221dc5ddef04f3bdf2fd74f75d9507e0514ef5c
Tim <tim@tim-ings.com>
2020-01-20 15:59:00 +0800

    Adds custom editor ts config

--- webpack.editor.config.js
+++ webpack.editor.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'awesome-typescript-loader?configFileName=./tsconfig.client.json',
+                use: 'awesome-typescript-loader?configFileName=./tsconfig.editor.json',
                 exclude: /node_modules/,
             },
             {
