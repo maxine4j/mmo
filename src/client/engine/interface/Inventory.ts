fc543d37fced24e584ba7596bf33b4a08608bb2b
Tim <tim@tim-ings.com>
2020-01-25 23:49:40 +0800

    Adds item dragging to inventory UI

--- src/client/engine/interface/Inventory.ts
+++ src/client/engine/interface/Inventory.ts
@@ -9,24 +9,67 @@ import SpriteAtlasImage from './components/SpriteAtlasImage';
 const slotSize = 32;
 const atlas = new SpriteAtlas('assets/icons/atlas.png');
 
+const slotBg = 'rgba(255,255,255,0.0)';
+const slotBgOver = 'rgba(255,255,255,0.2)';
+
 class InventorySlot extends Panel {
     public icon: SpriteAtlasImage;
     public item: Item;
     public slot: number;
+    public inventory: Inventory;
 
     public constructor(item: Item, slot: number, parent: Inventory, margin: number) {
         super(parent);
+        this.inventory = parent;
+        this.element.draggable = true;
         this.item = item;
         this.slot = slot;
 
-        this.icon = new SpriteAtlasImage(this, atlas, new Rectangle(5 * 32, 5 * 32, 32, 32));
-        this.icon.style.position = 'initial';
-        this.icon.width = 64;
-        this.icon.height = 64;
+        this.updateIcon();
 
         this.style.position = 'initial';
         this.style.display = 'inline-block';
         this.style.margin = `${margin}px`;
+        this.style.backgroundColor = slotBg;
+
+        this.element.addEventListener('dragstart', (ev: DragEvent) => {
+            ev.dataTransfer.setData('text/plain', slot.toString());
+        });
+        this.element.addEventListener('drop', (ev: DragEvent) => {
+            const data = ev.dataTransfer.getData('text');
+            this.inventory.swapSlots(this.slot, Number(data));
+            // @ts-ignore
+            ev.target.style.backgroundColor = slotBg;
+            ev.dataTransfer.clearData();
+            ev.preventDefault();
+        });
+        this.element.addEventListener('dragenter', (ev) => {
+            // @ts-ignore
+            ev.target.style.backgroundColor = slotBgOver;
+            ev.preventDefault();
+        });
+        this.element.addEventListener('dragleave', (ev) => {
+            // @ts-ignore
+            ev.target.style.backgroundColor = slotBg;
+            ev.preventDefault();
+        });
+        this.element.addEventListener('dragover', (ev) => ev.preventDefault());
+    }
+
+    private getIconOffset(id: number): [number, number] {
+        const stride = 16;
+        const x = id % stride;
+        const y = Math.floor(id / stride);
+        return [x, y];
+    }
+
+    public updateIcon(): void {
+        if (this.icon) this.icon.dispose();
+        const [x, y] = this.getIconOffset(this.item.data.icon);
+        this.icon = new SpriteAtlasImage(this, atlas, new Rectangle(x * 32, y * 32, 32, 32));
+        this.icon.style.position = 'initial';
+        this.icon.width = slotSize;
+        this.icon.height = slotSize;
     }
 }
 
@@ -35,7 +78,7 @@ export default class Inventory extends Panel {
     private readonly slotsPerRow: number = 4;
     private readonly margin: number = 5;
     private readonly slotSize: number = 32;
-    private slotsPerCol: number = this.slotCount / this.slotsPerRow;
+    private readonly slotsPerCol: number = this.slotCount / this.slotsPerRow;
     private slots: Map<number, InventorySlot> = new Map();
 
     public constructor() {
@@ -52,9 +95,13 @@ export default class Inventory extends Panel {
         for (let i = 0; i < 28; i++) {
             this.setSlot(i, new Item(<ItemDef>{
                 id: 0,
-                icon: 'assets/icons/atlas.png',
+                icon: 85,
             }));
         }
+    }
+
+    public swapSlots(a: number, b: number): void {
+
     }
 
     public setSlot(slot: number, item: Item): void {
--- src/common/ItemDef.ts
+++ src/common/ItemDef.ts
@@ -1,5 +1,5 @@
 export default interface ItemDef {
     id: number;
     name: string;
-    icon: string;
+    icon: number;
 }
