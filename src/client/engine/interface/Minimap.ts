d1cd4440021b1085bf8fa1af147c0de2fd8a5e77
Tim <tim@tim-ings.com>
2020-02-02 17:14:19 +0800

    Adds working minimap zooming

--- src/client/engine/interface/Minimap.ts
+++ src/client/engine/interface/Minimap.ts
@@ -1,7 +1,7 @@
 import Panel from './components/Panel';
 import { Frame } from './components/Frame';
 import World from '../World';
-import { TilePoint } from '../../../common/Point';
+import Slider from './components/Slider';
 
 export default class Minimap extends Panel {
     private world: World;
@@ -19,28 +19,44 @@ export default class Minimap extends Panel {
         this.style.top = '0';
         this.style.right = '0';
         this.style.backgroundColor = 'rgba(0,0,0,1)';
-        this.width = this.world.chunkWorld.chunkSize * 2;
-        this.height = this.world.chunkWorld.chunkSize * 2;
+        this.width = 100;
+        this.height = 100;
 
         this.canvas = document.createElement('canvas');
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         this.element.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
+        this.canvas.addEventListener('click', this.canvasClick.bind(this));
+
+        const slider = new Slider(this, 1, 10, this.scale, 0.1);
+        slider.style.position = 'initial';
+        slider.width = this.width;
+        slider.addEventListener('input', () => {
+            this.scale = slider.value;
+        });
+    }
+
+    private canvasClick(ev: MouseEvent): void {
+        const dx = ev.offsetX;
+        const dy = ev.offsetY;
+        console.log('Got a minimap click at', dx, dy);
     }
 
     public update(): void {
         if (this.world.player && this.world.player.position) {
-            this.ctx.clearRect(0, 0, 200, 200); // clear the canvas
-            const chunkSize = this.world.chunkWorld.chunkSize; // size of the chunks to render on the minimap
-            const playerPos = this.world.player.position; // the players positon to centre the minimap on
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear the canvas
 
             for (const [ccx, ccy, chunk] of this.world.chunkWorld.chunks) {
-                // TODO: only works with 2x scale
-                const x = (((chunkSize * ccx) + (chunkSize / 2) - playerPos.x) * this.scale) - ((chunkSize / 2) * this.scale);
-                const y = (((chunkSize * ccy) + (chunkSize / 2) - playerPos.y) * this.scale) - ((chunkSize / 2) * this.scale);
+                // calculate chunk position
+                const dw = this.world.chunkWorld.chunkSize * this.scale;
+                const dh = this.world.chunkWorld.chunkSize * this.scale;
+                const x = (dw * ccx) + (dw / 2);
+                const y = (dh * ccy) + (dh / 2);
+                const px = (this.world.player.position.x * this.scale) + dw - this.canvas.width / 2;
+                const py = (this.world.player.position.y * this.scale) + dh - this.canvas.height / 2;
 
-                this.ctx.drawImage(chunk.texture.image, x, y, chunkSize * this.scale, chunkSize * this.scale);
+                this.ctx.drawImage(chunk.texture.image, x - px, y - py, dw, dh);
             }
 
             this.ctx.fillStyle = 'red';
